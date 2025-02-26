af8a95594656cf631d684b66a512f87a78724eb2
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-02-26 18:51:37 -0300

    Inserção do console principal com a inclusão das duas variáveis iniciais e a variável de validação das operações

--- ProjetoCalculadora/Program.cs
+++ ProjetoCalculadora/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+Console.WriteLine("Projeto Calculadora Matheus e João");
+
+
+Console.WriteLine("Escreva o primeiro número");
+decimal primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Escreva o segundo numero");
+decimal segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a operação desejada: 1=> soma 2=> subtração 3=> multiplicação 4=> divisão ");
+int operacao = Convert.ToInt32(Console.ReadLine());
+
+
